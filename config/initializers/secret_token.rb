59245425606bea437ef90d8445821c87646b30fe
Phillip Gomez <gomez.phillip@gmail.com>
2012-03-04 21:45:00 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GomeztreeCom::Application.config.secret_token = 'c145419221426cc94642286cae979c9712730650615d557720635c78d66abb818e0625aae3f415d5bf0eb8f6ff7652b02c4e595eaf044f267b494b5c392cde41'
