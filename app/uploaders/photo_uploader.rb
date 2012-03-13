61b6a8cb283b53803bff652e8db176ffd92e32f2
Phillip Gomez <gomez.phillip@gmail.com>
2012-03-13 16:55:24 -0700

    Cleaned up Carrierwave test processing so no files are saved

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -13,7 +13,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    Rails.env.production? ? (primary_folder = "") : (primary_folder = "#{Rails.root}/spec/support/")
+    "#{primary_folder}uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
